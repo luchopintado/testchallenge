449d433bfa6369bd8877ecf8e0b51a1cfa78a544
Luis <luchopintado@gmail.com>
2019-07-22 08:34:23 -0500

    Select component

--- src/components/shared/Select.jsx
+++ src/components/shared/Select.jsx
@@ -1,33 +1,10 @@
 import React from 'react';
 
-const Select2 = ({ values, onChange, selectedValue }) => {
-    return (
-        <div className="select">
-            <select name="type" onChange={e => onChange(e.target.value)}>
-                {
-                    selectedValue
-                        ? <option key="option-0" value="">Seleccionar</option>
-                        : <option key="option-0" value="" selected>Seleccionar</option>
-
-                }
-                {
-                    values.map((val, idx) => {
-                        if (val === selectedValue) {
-                            return <option key={`option-${idx+1}`} value={val} selected>{val}</option>;
-                        } else {
-                            return <option key={`option-${idx+1}`} value={val} >{val}</option>;
-                        }
-                    })
-                }
-            </select>
-        </div>
-    );
-};
-
 const Select = ({ values, onChange, selectedValue }) => {
     return (
         <div className="select">
             <select defaultValue={selectedValue} name="type" onChange={e => onChange(e.target.value)}>
+                <option key="option-0" value="">Seleccionar</option>
                 {
                     values.map((val, idx) => {
                         return <option key={`option-${idx+1}`} value={val}>{val}</option>
---
+++ src/components/shared/Select.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { shallow, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Select from './Select';
+
+configure({ adapter: new Adapter() });
+
+describe('Select', () => {
+    const values = ['uno', 'dos', 'tres'];
+    const onChange = jest.fn();
+    const wrapper = shallow(<Select values={values} onChange={onChange} /> );
+
+    it('renders blank option', () => {
+        const resp = wrapper.find('option').at(0).text().includes('Seleccionar');
+        expect(resp).toEqual(true);
+    });
+
+    it('renders all options', () => {
+        // const values = ['uno', 'dos', 'tres'];
+        // const wrapper = mount(<Select values={values} /> );
+
+        expect(wrapper.find('option')).toHaveLength(values.length + 1);
+    });
+
+    it('calls onChange element', () => {
+        wrapper
+            .find('select')
+            .at(0)
+            .simulate('change', { target: { value: 'uno' } });
+
+        expect(onChange.mock.calls).toEqual([['uno']]);
+    });
+});
