27550c0cd234f512763722a485be986db0abeeef
Luis <luchopintado@gmail.com>
2019-07-21 19:20:05 -0500

    App

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Switch, Route } from 'react-router-dom';
+
+import DataShow from './pages/DataShow';
+import DataEdit from './pages/DataEdit';
+import RequestList from './pages/RequestList';
+import history from '../history';
+import Header from './shared/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router history={history}>
+        <>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={RequestList} />
+            <Route path="/request" exact component={RequestList} />
+            <Route path="/data/show" exact component={DataShow} />
+            <Route path="/data/edit" exact component={DataEdit} />
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
